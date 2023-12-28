be51ca533c38b1a75af1bf00605eeede4b7f0bdb
Evan Hensleigh <futuraprime@gmail.com>
2023-12-28 14:47:19 +0000

    Setup.

---
+++ index.js
@@ -0,0 +1,6 @@
+import { visit } from "unist-util-visit";
+import { h } from "hastscript";
+
+export default function rehypeFigureTitle(option) {
+  const className = (option && option.className) || "rehype-figure-title";
+}
---
+++ index.test.js
@@ -0,0 +1,5 @@
+import rehypeFigureTitle from "./index";
+
+test("expect 1+2 =3", () => {
+  expect(1 + 2).toBe(3);
+});
